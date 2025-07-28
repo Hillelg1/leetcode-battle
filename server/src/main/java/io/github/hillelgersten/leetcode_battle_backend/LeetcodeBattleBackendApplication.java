d00d5e1bd2b038969d8aa708afaa01bd7da84ef7
Hillel Gersten <Hillelgersten@gmail.com>
2025-07-28 12:00:54 -0400

    initialized front end with react and backend with spring boot

---
+++ server/src/main/java/io/github/hillelgersten/leetcode_battle_backend/LeetcodeBattleBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.hillelgersten.leetcode_battle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeBattleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeBattleBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/hillelgersten/leetcode_battle_backend/LeetcodeBattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hillelgersten.leetcode_battle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeBattleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
