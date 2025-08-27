a9e6460a96c3f76bfc8c23fa0dcab2625737cad8
Hillel Gersten <Hillelgersten@gmail.com>
2025-08-27 09:41:14 -0400

    added socket config

---
+++ server/src/main/java/io/github/hillelgersten/leetcode_battle_backend/configs/WebSocketConfig.java
@@ -0,0 +1,11 @@
+package io.github.hillelgersten.leetcode_battle_backend.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig {
+}
