7126104f0575ce414f34db62c69a60010baa19df
Hillelg1 <hillelgersten@gmail.com>
2026-02-06 11:56:08 +0200

    started match history modeling

---
+++ server/src/main/java/io/github/hillelgersten/leetcode_battle_backend/model/MatchHistory.java
@@ -0,0 +1,8 @@
+package io.github.hillelgersten.leetcode_battle_backend.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+public class MatchHistory {
+
+}
